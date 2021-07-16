0a341aa5243caf72fb60462942005de4286959e3
Evgeniy Sergeev <soersoft@gmail.com>
2021-07-16 11:09:53 +0300

    fix: add pattern to grep

--- scripts/nextver.sh
+++ scripts/nextver.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
 
-# show version  's/[\",]//g'
-echo $(npx standard-version --dry-run | grep release | awk '{ print $4 }' | sed 's/[\"v]//g' | tr -d '[[:space:]]')
+echo $(npx standard-version --dry-run | grep "release v" | awk '{ print $4 }' | sed 's/[\"v]//g' | tr -d '[[:space:]]')
